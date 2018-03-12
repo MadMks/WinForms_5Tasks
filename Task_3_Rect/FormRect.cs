6e4325300989233f6082e85c4cc141f0e35f29f2
MadMks <mks59k@gmail.com>
2018-03-12 22:41:02 +0200

    Task 3. Working. Written all the handlers.

--- Task_3_Rect/FormRect.cs
+++ Task_3_Rect/FormRect.cs
@@ -16,6 +16,15 @@ namespace Task_3_Rect
         /// Отступ от границ рабочей области формы.
         /// </summary>
         private const int _indent = 10;
+        /// <summary>
+        /// Нажат Ctrl.
+        /// </summary>
+        private bool IsPressedCtrl { get; set; }
+        /// <summary>
+        /// Координаты мыши.
+        /// </summary>
+        private MouseCoordinates MouseCoord { get; set; }
+
 
 
         public FormRect()
@@ -29,34 +38,49 @@ namespace Task_3_Rect
         // Обработчики.
 
 
+        /// <summary>
+        /// Обработчик загрузки приложения.
+        /// </summary>
         private void FormRect_Load(object sender, EventArgs e)
         {
             this.Text = "Задание 3 - прямоугольник";
-            this.Width = 450;
+            this.Width = 500;
             this.Height = 250;
+
+            IsPressedCtrl = false;
+            MouseCoord = new MouseCoordinates();
+            MouseCoord.X = 0;
+            MouseCoord.Y = 0;
 
             this.MouseClick += FormRect_MouseClick;
             this.MouseMove += FormRect_MouseMove;
             this.KeyDown += FormRect_KeyDown;
-
-            //this.
+            this.KeyUp += FormRect_KeyUp;
         }
 
-        
 
+        /// <summary>
+        /// Обработчик нажатия клавиши.
+        /// </summary>
         private void FormRect_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Modifiers == Keys.Control)
             {
-                MessageBox.Show("Close   !");
-            }
-            else if (e.Modifiers == Keys.Control)
-            {
-                //MessageBox.Show("Ctrl");
+                IsPressedCtrl = true;
             }
         }
 
 
+        /// <summary>
+        /// Обработчик отпускания клавиши.
+        /// </summary>
+        private void FormRect_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Modifiers == Keys.Control)
+            {
+                IsPressedCtrl = false;
+            }
+        }
 
 
         /// <summary>
@@ -64,45 +88,90 @@ namespace Task_3_Rect
         /// </summary>
         private void FormRect_MouseMove(object sender, MouseEventArgs e)
         {
-            this.Text = $"Текущие координаты мыши: X={e.X}, Y={e.Y}";
+            if (IsCoordinatesMouseHaveChanged(e) == true)
+            {
+                this.Text = $"Текущие координаты мыши: X={e.X}, Y={e.Y}";
+            }
+
+            // Запомним последние изменения координат мыши.
+            MouseCoord.X = e.X;
+            MouseCoord.Y = e.Y;
         }
 
-
+        
         /// <summary>
         /// Обработчик клика мышки.
         /// </summary>
         private void FormRect_MouseClick(object sender, MouseEventArgs e)
         {
-
-
-            //if (IsLeftMouseButtonAndCtrlClick(e))
-            //{
-            //    MessageBox.Show("777");
-
-            //}
-            //else if (LeftMouseButtonClick(e))
-            //{
-            //    if (IsPointOnTheBoundaryOfARectangle(e))
-            //    {
-            //        ShowMessagePointLocation("на границе прямоугольника");
-            //    }
-            //    else if (IsAPointOutsideTheRectangle(e))
-            //    {
-            //        ShowMessagePointLocation("снаружи прямоугольника");
-            //    }
-            //    else if (IsAPointInsideTheRectangle(e))
-            //    {
-            //        ShowMessagePointLocation("внутри прямоугольника");
-            //    }
-            //}
-            //else if (RightMouseButtonClick(e))
-            //{
-
-            //}
-
+            if (IsLeftMouseButtonAndCtrlClick(e))
+            {
+                this.Close();
+            }
+            else if (LeftMouseButtonClick(e))
+            {
+                if (IsPointOnTheBoundaryOfARectangle(e))
+                {
+                    ShowMessagePointLocation("на границе прямоугольника");
+                }
+                else if (IsAPointOutsideTheRectangle(e))
+                {
+                    ShowMessagePointLocation("снаружи прямоугольника");
+                }
+                else if (IsAPointInsideTheRectangle(e))
+                {
+                    ShowMessagePointLocation("внутри прямоугольника");
+                }
+            }
+            else if (RightMouseButtonClick(e))
+            {
+                this.Text = "Клиентская область окна: " 
+                    + $"Ширина={this.ClientRectangle.Width}, " 
+                    + $"Высота={this.ClientRectangle.Height}";
+            }
         }
 
+
+        // Методы.
+
+        
+        /// <summary>
+        /// Координаты мыши изменились.
+        /// </summary>
+        /// <returns>true если изменились.</returns>
+        private bool IsCoordinatesMouseHaveChanged(MouseEventArgs e)
+        {
+            if (e.X != MouseCoord.X || e.Y != MouseCoord.Y)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        /// <summary>
+        /// Нажата ЛКМ и CTRL.
+        /// </summary>
+        /// <param name="e">Событие мыши.</param>
+        /// <returns>true если нажаты ЛКМ и Ctrl.</returns>
         private bool IsLeftMouseButtonAndCtrlClick(MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && IsPressedCtrl == true)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+
+        /// <summary>
+        /// Нажата ЛКМ.
+        /// </summary>
+        /// <param name="e">Событие мыши.</param>
+        /// <returns>true если нажата ЛКМ.</returns>
+        private bool LeftMouseButtonClick(MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
             {
@@ -112,18 +181,21 @@ namespace Task_3_Rect
             return false;
         }
 
+
+        /// <summary>
+        /// Нажата ПКМ.
+        /// </summary>
+        /// <param name="e">Событие мыши.</param>
+        /// <returns>true если нажата ПКМ.</returns>
         private bool RightMouseButtonClick(MouseEventArgs e)
         {
-            throw new NotImplementedException();
+            if (e.Button == MouseButtons.Right)
+            {
+                return true;
+            }
+
+            return false;
         }
-
-        private bool LeftMouseButtonClick(MouseEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        // Методы.
 
 
         /// <summary>
---
+++ Task_3_Rect/MouseCoordinates.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_Rect
+{
+    /// <summary>
+    /// Класс "Координаты мыши".
+    /// </summary>
+    public  class MouseCoordinates
+    {
+        public MouseCoordinates() { }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
